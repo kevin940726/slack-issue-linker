521bfc5f61418e487c9c5b3b9c0a860db64269d2
kevin940726 <kevin830726@gmail.com>
2016-07-14 19:18:33 +0800

    split token into confidential

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { slackbot } = require('botkit');
 const firebase = require('firebase');
+const token = require('./slackToken.json').token;
 
 firebase.initializeApp({
 	serviceAccount: './serviceAccountCredentials.json',
@@ -13,13 +14,31 @@ const controller = slackbot({
 	debug: false,
 });
 
-controller.spawn({
-	token: 'xoxb-59685306928-TttmiDAVewuj5vuIhsJ45N3G',
-}).startRTM();
+controller.spawn({ token }).startRTM();
+
+controller.hears('^issue set-url ([^\\s]+) ?([^\\s]+)?$', ['message_received', 'ambient'], (bot, message) => {
+	const channel = message.channel;
+	let repo = 'default';
+	let url = message.match[1];
+
+	if (message.match[2]) {
+		repo = message.match[1];
+		url = message.match[2];
+	}
+
+	ref.update({
+		[`${channel}/${repo}`]: url.slice(1, url.length - 1),
+	});
+
+	bot.reply(message, `set ${repo} to ${url}.`);
+});
 
 controller.hears('([^\\s]+)?#(\\d+)', ['message_received', 'direct_mention', 'ambient'], (bot, message) => {
+	const channel = message.channel;
 	const repo = message.match[1] || 'default';
 	const issue = message.match[2];
 
-	bot.reply(message, `http://nas25lol.myqnapcloud.com:10088/${repo}/issues/${issue}`);
+	ref.child(channel).child(repo).once('value', snapshot => {
+		bot.reply(message, `${snapshot.val()}/issues/${issue}`);
+	});
 });
